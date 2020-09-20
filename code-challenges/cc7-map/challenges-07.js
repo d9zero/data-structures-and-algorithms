38cf58c7aa3d08e82b67d8ce591ed85385d98272
d9zero <augurson90@gmail.com>
2020-09-20 10:17:51 -0700

    cookies and milk

--- code-challenges/cc7-map/challenges-07.js
+++ code-challenges/cc7-map/challenges-07.js
@@ -37,6 +37,10 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  let outputArr = [];
+  arr.forEach(int => {
+    outputArr.push(Math.pow(2, int));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,6 +51,10 @@ Write a function named forEachTwoToThe that produces the same output as your for
 
 const forEachTwoToThe = (arr) => {
   // Solution code here...
+  let outputArr = [];
+  arr.forEach(int => {
+    outputArr.push(Math.pow(2, int));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -57,6 +65,7 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map(int => Math.pow(2, int));
 };
 
 /* ------------------------------------------------------------------------------------------------
